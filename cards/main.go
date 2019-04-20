01067af1b005a1fe8df01ac7da7c7672169737b8
Andrey <ramirez.jag00@gmail.com>
2019-04-20 16:21:40 +0800

    Write example of basic type conversion for byte slice

--- cards/main.go
+++ cards/main.go
@@ -1,13 +1,21 @@
 package main
 
+import "fmt"
+
 func main() {
-  cards := newDeck()
+  // cards := newDeck()
 
   // variable using print method for type deck
   // cards.print()
 
-  hands, remainingCards := deal(cards, 5)
+  // hands, remainingCards := deal(cards, 5)
+  //
+  // hands.print()
+  // remainingCards.print()
 
-  hands.print()
-  remainingCards.print()
+  greeting := "Hello there!"
+  fmt.Println([]byte(greeting))
+  // this is the result of "type conversion"
+  // basically turning string characters into its value in ascii (byte) and putting results in slice
+  // [72 101 108 108 111 32 116 104 101 114 101 33]
 }
