b4052d75f2174ad5a024be79c50ea6e3e998c8b1
Andrey <ramirez.jag00@gmail.com>
2019-04-20 15:53:11 +0800

    initial commit

---
+++ cards/deck.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+// create a new type of of 'deck'
+// which a slice of strings
+// there is no class in golang
+type deck []string
+
+// this will create a new deck type of cards that will mock all values and suits
+func newDeck() deck {
+  cards := deck{}
+
+  cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+  cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+  // iterete through the suits and values to mock the whole deck of cards
+  // first _ is saying that we are not using the index i so we replace it with an _
+  // second _ is saying that we are not using the index j so we replace it with an _
+  for _, suit := range cardSuits {
+    for _, value := range cardValues {
+      cards = append(cards, value+" of "+suit)
+    }
+  }
+
+  return cards
+}
+
+// any variable of type deck now gets access to the "print" method
+// d is the 'receiver' values of the variable used or actual copy of the assigned deck
+// there is no this in golang
+// by convention we use 1-3 letter abbreviation of the type, but you can always change it to anything
+func (d deck) print() {
+  for i, card := range d {
+    fmt.Println(i, card)
+  }
+}
---
+++ cards/main.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+  cards := newDeck()
+
+  // variable using print method for type deck
+  cards.print()
+}
