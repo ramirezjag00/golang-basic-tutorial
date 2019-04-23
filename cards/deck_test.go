951a2f244a841f4f0934224e42ffa9c8dec969a3
Andrey <ramirez.jag00@gmail.com>
2019-04-23 18:50:42 +0800

    Add other type deck method, tests: saveToFile and newFileFromDeck

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -5,8 +5,9 @@ package main
 // all go knows is that we ran a function and there might be something wrong with it
 // in go, test fileNames should be name_test.go by convention
 import "testing"
+import "os"
 
-// UpperCase test func to make it distinguishable from normal methods
+// UpperCase Test func to make it distinguishable from normal methods
 // type *testing.T of the received t argument
 func TestNewDeck(t *testing.T) {
   // d is instance or receiver of newDeck
@@ -26,4 +27,21 @@ func TestNewDeck(t *testing.T) {
   if d[len(d) - 1] != "King of Clubs" {
     t.Errorf("Expected last card in the deck to be King of Clubs, but got %v", d[len(d) - 1])
   }
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+  // _ doesn't mean anything, this is just a temporary file indicator
+  // before we start the, remove _decktesting file in the directory
+  os.Remove("_decktesting")
+
+  deck := newDeck()
+  deck.saveToFile("_decktesting")
+
+  loadedDeck := newDeckFromFile("_decktesting")
+
+  if len(loadedDeck) != 52 {
+    t.Errorf("Expected 52 cards, but got %v", len(loadedDeck))
+  }
+
+  os.Remove("_decktesting")
 }
