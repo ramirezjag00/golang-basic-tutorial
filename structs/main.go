22b13c20cb0021f76aff020251a6afef5aa0aa49
Andrey Ramirez <ramirez.jag00@gmail.com>
2019-05-01 17:09:32 +0800

    Added desctructuring in embedded struct

--- structs/main.go
+++ structs/main.go
@@ -7,22 +7,22 @@ type contactInfo struct {
 	zipCode int
 }
 type person struct {
-	firstName string
-	lastName  string
-	contact   contactInfo
+	firstName   string
+	lastName    string
+	contactInfo // contactInfo contactInfo
 }
 
 func main() {
 	jim := person{
 		firstName: "Jimmy",
 		lastName:  "Neutron",
-		contact: contactInfo{
+		contactInfo: contactInfo{
 			email:   "jimmy.neutron@email.com",
 			zipCode: 12345,
 		},
 	}
 	fmt.Printf("First Name: %+v", jim.firstName)
 	fmt.Printf("Last Name: %+v", jim.lastName)
-	fmt.Printf("E-mail: %+v", jim.contact.email)
-	fmt.Printf("Zipcode: %+v", jim.contact.zipCode)
+	fmt.Printf("E-mail: %+v", jim.contactInfo.email)
+	fmt.Printf("Zipcode: %+v", jim.contactInfo.zipCode)
 }
