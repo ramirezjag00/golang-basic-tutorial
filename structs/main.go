ec165a9b77d54b9a2f6880cbdb3873951bedb0cd
Andrey Ramirez <ramirez.jag00@gmail.com>
2019-05-01 17:06:25 +0800

    Embedded Struct

--- structs/main.go
+++ structs/main.go
@@ -2,31 +2,27 @@ package main
 
 import "fmt"
 
-// struct == hash == object == dictionary
+type contactInfo struct {
+	email   string
+	zipCode int
+}
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	// sample 1
-	// person1 := person{ firstName: "Drey", lastName: "Dev" }
-	// fmt.Println(person1.firstName) // "Drey"
-
-	// sample 2
-	// & yields pointer to the struct
-	// samePerson := &person1
-	// fmt.Println(samePerson.firstName) // "Drey"
-
-	// sample 3
-	var person1 person
-	// string interpolation
-	// print empty struct
-	// {firstName: lastName:}%
-	fmt.Printf("%+v", person1)
-
-	person1.firstName = "Drey"
-	person1.lastName = "Dev"
-	fmt.Println(person1)
-	// {Drey Dev}
+	jim := person{
+		firstName: "Jimmy",
+		lastName:  "Neutron",
+		contact: contactInfo{
+			email:   "jimmy.neutron@email.com",
+			zipCode: 12345,
+		},
+	}
+	fmt.Printf("First Name: %+v", jim.firstName)
+	fmt.Printf("Last Name: %+v", jim.lastName)
+	fmt.Printf("E-mail: %+v", jim.contact.email)
+	fmt.Printf("Zipcode: %+v", jim.contact.zipCode)
 }
