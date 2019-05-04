bb1415d27862cd2ced2b70a4e4f54781435b0b2e
Andrey Ramirez <ramirez.jag00@gmail.com>
2019-05-04 13:24:36 +0800

    Add books rest api - add new book

--- books/main.go
+++ books/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -48,7 +50,12 @@ func getBook(w http.ResponseWriter, r *http.Request) {
 
 // Create New Book
 func createBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(10000000)) // Mock ID, not safe
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
 }
 
 // Update a Book
